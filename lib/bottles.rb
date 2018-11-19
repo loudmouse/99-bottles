bf1aa54a04465602ffe68c25466197bec4bcced4
Nolan Neeley <nolanneeley@gmail.com>
2018-11-19 17:26:16 -0600

    deleted old code

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,33 +1,4 @@
 class Bottles
 
-  attr_reader :of_what, :on_what
 
-  def initialize
-    @of_what = "of beer"
-    @on_what = "on the wall"
-  end
-
-  def pluralize(n)
-    if n != 1
-      return 's'
-    end
-  end
-
-  # the song method sends the verses messages to the implicit receiver (self), passing it two arguments.
-  def song
-    verses(99, 0)
-  end
-
-  def verses(hi,lo)
-    hi.downto(lo).map {|n| verse(n) }.join("\n")
-  end
-
-  def verse(n)
-    "#{n == 0 ? 'No more' : n} bottle#{pluralize(n)}" +
-    " #{of_what} #{on_what}, " +
-    "#{n == 0 ? 'no more' : n} bottle#{pluralize(n)} #{of_what}.\n" +
-    "#{n > 0 ? "Take #{n > 1 ? 'one' : 'it'} down and pass it around"
-    : "Go to the store and buy some more"}, " +
-    "#{n-1 < 0 ? 99 : n-1 == 0 ? 'no more' : n-1} bottle#{pluralize(n-1)}"+ " #{of_what} #{on_what}.\n"
-  end
 end
