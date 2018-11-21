7fa646af2d507bcd3df48effe9619a43d2152c99
Nolan Neeley <nolanneeley@gmail.com>
2018-11-21 16:20:30 -0600

    tests and passing code for 3 verse types

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,38 +1,70 @@
 class Bottles
-
-  def song
-    verses(99,0)
-  end
-
-  def verses(starting,ending)
-    starting.downto(ending).map do |number|
-      verse(number)
-    end.join("\n")
-  end
-
   def verse(number)
-    case number
-    when 0
-      "No more bottles of beer on the wall, " +
-      "no more bottles of beer.\n" +
-      "Go to the store and buy some more, " +
-      "99 bottles of beer on the wall.\n"
-    when 1
-      "#{number} bottle of beer on the wall, " +
-      "#{number} bottle of beer.\n" +
-      "Take it down and pass it around, " +
-      "no more bottles of beer on the wall.\n"
-    when 2
-      "#{number} bottles of beer on the wall, " +
-      "#{number} bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "#{number - 1} bottle of beer on the wall.\n"
+    if number == 2
+    <<-VERSE
+    2 bottles of beer on the wall, 2 bottles of beer.
+    Take one down and pass it around, 1 bottle of beer on the wall.
+    VERSE
+    elsif number == 1
+    <<-VERSE
+    1 bottle of beer on the wall, 1 bottle of beer.
+    Take it down and pass it around, no more bottles of beer on the wall.
+    VERSE
     else
-      "#{number} bottles of beer on the wall, " +
-      "#{number} bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "#{number - 1} bottles of beer on the wall.\n"
+    <<-VERSE
+    #{number} bottles of beer on the wall, #{number} bottles of beer.
+    Take one down and pass it around, #{number-1} bottles of beer on the wall.
+    VERSE
     end
   end
-
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Bottles
+#   def song
+#     verses(99,0)
+#   end
+#
+#   def verses(starting,ending)
+#     starting.downto(ending).map do |number|
+#       verse(number)
+#     end.join("\n")
+#   end
+#
+#   def verse(number)
+#     case number
+#     when 0
+#       "No more bottles of beer on the wall, " +
+#       "no more bottles of beer.\n" +
+#       "Go to the store and buy some more, " +
+#       "99 bottles of beer on the wall.\n"
+#     when 1
+#       "#{number} bottle of beer on the wall, " +
+#       "#{number} bottle of beer.\n" +
+#       "Take it down and pass it around, " +
+#       "no more bottles of beer on the wall.\n"
+#     when 2
+#       "#{number} bottles of beer on the wall, " +
+#       "#{number} bottles of beer.\n" +
+#       "Take one down and pass it around, " +
+#       "#{number - 1} bottle of beer on the wall.\n"
+#     else
+#       "#{number} bottles of beer on the wall, " +
+#       "#{number} bottles of beer.\n" +
+#       "Take one down and pass it around, " +
+#       "#{number - 1} bottles of beer on the wall.\n"
+#     end
+#   end
+# end
